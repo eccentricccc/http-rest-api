b6cffd935488632aa313f6dee34f708e21558a70
mmms <shynar.mars30@gmail.com>
2021-05-24 11:16:14 +0500

    first commit

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+
+	"github.com/eccentricccc/http-rest-api/internal/app/apiserver"
+	// _ "github.com/lib/pq"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
